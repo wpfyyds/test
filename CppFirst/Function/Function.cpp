5a641d3a713cb93fbac6777e377411a6a984a95f
wpfyyds <2811988744@qq.com>
2025-05-21 19:29:03 +0800

    函数相关

---
+++ CppFirst/Function/Function.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include "SetPrint.h"
+using namespace std;
+
+int a = 10;//全局变量
+void fun(int num, int current);
+
+int main()
+{
+    int a = 10;//局部变量
+    cout<<printFf(1, 2)<<endl;
+    fun(1,200);
+}
+
+void fun(int num,int current) {
+    if (num>current)
+        return;
+    cout << num << ",";
+    fun(num + 1, current);
+}
+
+
+
---
+++ CppFirst/Function/useprint.cpp
@@ -0,0 +1,4 @@
+#include "SetPrint.h"
+int printFf(int x, int y) {
+    return x + y;
+}
